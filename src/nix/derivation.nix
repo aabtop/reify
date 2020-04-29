146f8a4839d56cf740c60ab34fa7bcbb2f50a329
Andrew Top <aabtop@gmail.com>
2020-04-28 22:26:26 -0700

    Ensure that clang-format is in the environment.
    Also, add vscode extension language support for Nix.

--- src/nix/derivation.nix
+++ src/nix/derivation.nix
@@ -1,9 +1,9 @@
-{stdenv, cmake, memdesc, v8, reify-interface}:
+{stdenv, cmake, memdesc, v8, reify-interface, clang-tools}:
  
 stdenv.mkDerivation {
   name = "reify";
   
-  nativeBuildInputs = [ cmake memdesc reify-interface ];
+  nativeBuildInputs = [ cmake memdesc reify-interface clang-tools];
   buildInputs = [ v8 ];
 
   src = ../.;
