2e77fe599f7d9d80d67cba1a8c18ca733ffbba54
Andrew Top <aabtop@gmail.com>
2020-05-16 00:14:14 +0000

    Polish up sample scripts a bit.

--- projects/hypo/src/example_scripts/jeep.ts
+++ projects/hypo/src/example_scripts/jeep.ts
@@ -1,30 +1,17 @@
 import * as h from 'hypo';
 
-export function Eye(center: h.Vec2) {
-  let eye_area = h.Intersection2AsRegion2(h.Intersection2({
-    regions: [
-      h.CircleAsPolygonAsRegion2(h.CircleAsPolygon({
-        circle: {radius: 10, center: [center[0], center[1] - 7.5]},
-        num_points: 100
-      })),
-      h.CircleAsPolygonAsRegion2(h.CircleAsPolygon({
-        circle: {radius: 10, center: [center[0], center[1] + 7.5]},
-        num_points: 100
-      }))
-    ]
-  }));
+let jeep_wheel = h.Cylinder(0.35, 0.15, 20);
 
-  let pupil = h.CircleAsPolygonAsRegion2(h.CircleAsPolygon(
-      {circle: {radius: 2, center: [center[0], center[1]]}, num_points: 50}));
+let my_jeep = h.Region3UnionAsRegion3(h.Region3Union({
+  regions: [
+    h.TranslatedRegion3(jeep_wheel, [1.0, 0.0, -0.5]),
+    h.TranslatedRegion3(jeep_wheel, [1.0, 0.0, -0.5]),
+    h.TranslatedRegion3(jeep_wheel, [1.0, 0.0, 0.5]),
+    h.TranslatedRegion3(jeep_wheel, [-1.0, 0.0, 0.5]),
+    h.TranslatedRegion3(jeep_wheel, [-1.0, 0.0, -0.5]),
+  ]
+}));
 
-  return h.Difference2AsRegion2(h.Difference2({a: eye_area, b: pupil}))
-}
-
-export function Test() {
-  let rect = h.RectangleAsRegion2(
-      h.Rectangle({left: 0, top: 20, right: 40, bottom: 0}));
-
-  let eyes =
-      h.Union2AsRegion2(h.Union2({regions: [Eye([10, 10]), Eye([30, 10])]}));
-  return h.Difference2AsRegion2(h.Difference2({a: rect, b: eyes}));
+export function Jeep() {
+  return my_jeep;
 }
