278b2c9196b96a6a0aa7d1f5abf929c2c69eb37b
Andrew Top <aabtop@gmail.com>
2020-05-19 23:14:37 -0700

    Fix build in multi user environments.
    We were pulling down node_modules ad-hoc and Nix didn't know
    what was happening.  We are allowed to arbitrarily access
    the network in single user Nix, but not multi user.

--- src/tsc_wrapper/default.nix
+++ src/tsc_wrapper/default.nix
@@ -1,10 +1,35 @@
-{runCommand, nodejs}:
+{runCommand, nodePackages}:
 
-runCommand "reify-tsc-wrapper" { buildInputs = [nodejs]; src = ./.; } ''
-  echo `pwd`
-  cp -r $src/* .
-  export HOME=`pwd`/home
-  npm install
-  npx webpack
-  cp dist/tsc_wrapper.js $out
-''
+let
+  # First we copy the package.json out and isolate it, so that we can fetch
+  # depending only on package.json and nothing else.  This is done because
+  # fetching the dependencies can take a very long time, and it's nice to not
+  # have to do this if we're just changing local source code.
+  isolatePackageJson =
+    runCommand "reify-package-copy" { src = ./package.json; } "cp $src $out";
+  setupNode2Nix =
+    runCommand "reify-setup-node-2-nix" {
+          buildInputs = [nodePackages.node2nix]; inherit isolatePackageJson;
+        }
+        ''
+          mkdir -p $out
+          cd $out
+          cp $isolatePackageJson package.json
+          node2nix
+        '';
+  node2NixDerivation = (import setupNode2Nix {}).package;
+in
+  runCommand
+      "generate-tsc_wrapper-binary"
+      {
+        inherit node2NixDerivation;
+        src = ./.;
+      }
+      # Overlay our original source code over top the fetched node_modules
+      # directory, and then call webpack on it.
+      ''
+        cp --no-preserve=mode,ownership -r $src/* .
+        cp -r $node2NixDerivation/lib/node_modules/tsc_wrapper/* .
+        node_modules/.bin/webpack
+        cp dist/tsc_wrapper.js $out
+      ''
