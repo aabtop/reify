52a239c8f2c0f17f4e5be50f627462875dc3b3fb
Andrew Top <aabtop@gmail.com>
2020-05-21 02:03:39 +0000

    Use a release build of V8 in the package.

--- projects/hypo/src/default.nix
+++ projects/hypo/src/default.nix
@@ -6,6 +6,15 @@ let
   reifyProject = callPackage (reifySrc + /nix/project.nix) { inherit inputInterfacePath; };
   reify = callPackage (reifySrc + /nix/derivation.nix) { inherit reifyProject; };
 
+
+  v8_no_icu = v8.overrideAttrs (attrs: {
+    buildInputs = [ glib ];
+    gnFlags =
+      (lib.remove "is_debug=true" attrs.gnFlags) ++ [
+          "v8_enable_i18n_support=false"
+          "is_debug=false"
+        ];
+  });
 in
   stdenv.mkDerivation {
     name = "hypo";
@@ -13,7 +22,7 @@ in
     inherit reifyProject;
     nativeBuildInputs = [cmake reifyProject];
     buildInputs = [
-      v8
+      v8_no_icu
       pkgsStatic.cgal_5
       pkgsStatic.gmp
       pkgsStatic.mpfr
