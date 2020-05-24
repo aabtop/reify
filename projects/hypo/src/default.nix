10479ddd3ba5d38fba6597f95cfd9a936c8c3019
Andrew Top <aabtop@hotmail.com>
2020-05-24 18:53:58 +0000

    Build V8 with "is_official_build" flag.
    Also provide a more formal choice between v8 release and v8
    debug libraries.

--- projects/hypo/src/default.nix
+++ projects/hypo/src/default.nix
@@ -6,14 +6,24 @@ let
   reifyProject = callPackage (reifySrc + /nix/project.nix) { inherit inputInterfacePath; };
   reify = callPackage (reifySrc + /nix/derivation.nix) { inherit reifyProject; };
 
-
   v8_no_icu = v8.overrideAttrs (attrs: {
     buildInputs = [ glib ];
     gnFlags =
-      (lib.remove "is_debug=true" attrs.gnFlags) ++ [
+        (lib.remove "v8_enable_i18n_support=true" attrs.gnFlags) ++ [
           "v8_enable_i18n_support=false"
-          "is_debug=false"
         ];
+  });
+  v8_release = v8_no_icu.overrideAttrs (attrs: {
+    gnFlags =
+      (lib.remove "is_debug=true" (
+              lib.remove "is_official_build=false" attrs.gnFlags)) ++ [
+          "is_debug=false"
+          "is_official_build=true"
+          "is_cfi=false"
+        ];
+  });
+  v8_debug = v8_no_icu.overrideAttrs (attrs: {
+    dontStrip = true;
   });
 in
   stdenv.mkDerivation {
@@ -22,7 +32,7 @@ in
     inherit reifyProject;
     nativeBuildInputs = [cmake reifyProject];
     buildInputs = [
-      v8_no_icu
+      v8_release
       pkgsStatic.cgal_5
       pkgsStatic.gmp
       pkgsStatic.mpfr
