5af9d2c5e54c3932c5ae03f41a711012204ddfc6
Andrew Top <aabtop@gmail.com>
2020-05-28 18:08:11 -0700

    Simplify build.sh to assume a normal linux env.
    Now that WSL2 is generally available, we don't have to do any
    crazy stuff to make our build.sh script work there versus
    native Linux.

--- build.sh
+++ build.sh
@@ -2,12 +2,12 @@
 
 THIS_SCRIPT_LOCATION="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-HOST_SRC_DIR_SHELL=${THIS_SCRIPT_LOCATION}
-HOST_OUT_DIR_SHELL=${THIS_SCRIPT_LOCATION}/docker_out
+HOST_SRC_DIR=${THIS_SCRIPT_LOCATION}
+HOST_OUT_DIR=${THIS_SCRIPT_LOCATION}/docker_out
 DOCKERFILE_DIRECTORY=${THIS_SCRIPT_LOCATION}
 
 # Make sure the host output directory exists.
-mkdir -p $HOST_OUT_DIR_SHELL
+mkdir -p $HOST_OUT_DIR
 
 RUN_COMMAND="/src/src/build_in_docker.sh"
 
@@ -21,33 +21,15 @@ r) RUN_COMMAND=${OPTARG};;
 esac
 done
 
-# This logic exists to ensure that we can run Docker fine from not just standard
-# Linux environments but also WSL and MSYS.
-if [ -x "$(command -v docker.exe)" ] && [ -x "$(command -v wslpath)" ]; then
-    # Presumably running from WSL with Docker Desktop for Windows.
-    DOCKER_COMMAND=docker.exe
-    HOST_SRC_DIR=$(wslpath -aw $HOST_SRC_DIR_SHELL)
-    HOST_OUT_DIR=$(wslpath -aw $HOST_OUT_DIR_SHELL)
-    DOCKERFILE_DIRECTORY=$(wslpath -aw $DOCKERFILE_DIRECTORY)
-elif [ -x "$(command -v docker)" ]; then
-    DOCKER_COMMAND=docker
-    HOST_SRC_DIR=${HOST_SRC_DIR_SHELL}
-    HOST_OUT_DIR=${HOST_OUT_DIR_SHELL}
-    DOCKERFILE_DIRECTORY=${DOCKERFILE_DIRECTORY}
-else
-    echo "Cannot find docker in path."
+if [ ! -x "$(command -v docker)" ]; then
+    echo "Cannot find docker in path, please install docker."
     exit 1
-fi
-
-#If we're running on MSYS we need to prepend "winpty" to the docker command.
-if [ "$(expr substr $(uname -s) 1 5)" == "MINGW" ]; then
-    DOCKER_COMMAND="winpty ${DOCKER_COMMAND}"
 fi
 
 # Make sure the Docker container image containing the build environment is
 # up to date and then run the actual build command inside the container.
-${DOCKER_COMMAND} build -t reify-build-env ${DOCKERFILE_DIRECTORY} && \
-${DOCKER_COMMAND} run \
+docker build -t reify-build-env ${DOCKERFILE_DIRECTORY} && \
+docker run \
     --rm \
     --name reify-build-env-instance \
     -it \
