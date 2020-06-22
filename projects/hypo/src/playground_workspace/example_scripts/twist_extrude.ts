ede6d1b9f352fdfdd13920fabdef00962629cc4a
Andrew Top <aabtop@gmail.com>
2020-06-22 02:23:31 -0700

    Add cool twist_ring example.

--- projects/hypo/src/playground_workspace/example_scripts/twist_extrude.ts
+++ projects/hypo/src/playground_workspace/example_scripts/twist_extrude.ts
@@ -1,17 +1,12 @@
 import * as h from 'hypo';
 
 export function Main() {
-  const RADIUS = 1;
-  const HEIGHT = 8;
-  const TWIST = 360;
-  const LAYERS = 64;
-
-  let crossSection = h.Box2({corners: [[-RADIUS, RADIUS], [RADIUS, -RADIUS]]});
+  let crossSection = h.Box2({corners: [[-1, -1], [1, 1]]});
 
   return h.TwistExtrudeFromZPlane({
     source: crossSection,
-    height: HEIGHT,
-    twist_amount_in_degrees: TWIST,
-    num_slices: LAYERS
+    height: 8,
+    twist_amount_in_degrees: 360,
+    num_slices: 64
   });
 }
