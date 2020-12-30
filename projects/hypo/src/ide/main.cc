d19809c6729d471a753c04c087fa9fda921f4165
Andrew Top <aabtop@gmail.com>
2020-12-29 19:33:01 -0800

    Polish up the Monaco UI.

--- projects/hypo/src/ide/main.cc
+++ projects/hypo/src/ide/main.cc
@@ -4,6 +4,7 @@
 #include "src/ide/mainwindow.h"
 
 int main(int argc, char* argv[]) {
+  QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
   QApplication a(argc, argv);
   MainWindow w;
   w.show();
--- src/monaco_wrapper/src/index.ts
+++ src/monaco_wrapper/src/index.ts
@@ -6,6 +6,11 @@ if (!container) {
 }
 
 monaco.editor.create(container, {
+  // Without explicitly setting this I observed an extra space being added
+  // whenever I selected something.
+  renderWhitespace: "none",
+  // We want the editor to be automatically resized as its container is resized.
+  automaticLayout: true,
   value: [
     'function x() {',
     '\tconsole.log("Hello world!");',
