ffbf158e350787aa9aad2e1b8642e29354864dcb
Andrew Top <aabtop@gmail.com>
2021-02-19 22:35:27 -0800

    Fix up spiral beams so it fits in the viewport.

--- projects/hypo/src/playground_workspace/example_scripts/spiral_beams.ts
+++ projects/hypo/src/playground_workspace/example_scripts/spiral_beams.ts
@@ -1,29 +1,38 @@
 import * as h from 'hypo';
 
+const beamRadius = 0.25;
+const beamLength = 4.0;
+const twistIncrementInDegrees = 36;
+
 function Beam(radius: number, length: number): h.Region3 {
   return h.Box3({
     corners: [[-radius, -radius, -length / 2], [radius, radius, length / 2]]
   });
 }
 
+function StackHeight(stackSize: number): number {
+  return beamRadius * 2.0 * stackSize;
+}
+
 function SpiralStack(stackSize: number): h.Region3[] {
   if (stackSize == 0) return [];
-
-  const beamRadius = 5.0;
-  const beamLength = 80.0;
-  const twistIncrementInDegrees = 36;
 
   return [
     h.Transform3({
       source: Beam(beamRadius, beamLength),
       transform: h.MMul4(
-          h.Translate3([2 * beamRadius * stackSize, 0, 0]),
-          h.Rotate3X(twistIncrementInDegrees * stackSize))
+        h.Translate3([0, 2 * beamRadius * stackSize, 0]),
+        h.Rotate3Y(twistIncrementInDegrees * stackSize))
     }),
     ...SpiralStack(stackSize - 1)
   ];
 }
 
 export function Main() {
-  return h.Union3({regions: SpiralStack(10)});
+  const stackSize = 10;
+
+  return h.Transform3({
+    source: h.Union3({ regions: SpiralStack(stackSize) }),
+    transform: h.Translate3([0, -StackHeight(stackSize) / 2.0, 0])
+  });
 }
