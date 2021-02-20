9ff0242e8f7f8c973d9f26effa7527b47a87dac2
Andrew Top <aabtop@gmail.com>
2021-02-19 22:01:54 -0800

    Fix bug where reloading a project would crash.
    Turns out V8 doesn't like being disposed of and then
    re initialized in the same process.

--- src/idt/targets/typescript_cpp_v8/core/global_initialization.cc
+++ src/idt/targets/typescript_cpp_v8/core/global_initialization.cc
@@ -5,36 +5,65 @@
 
 #include <cassert>
 #include <memory>
+#include <mutex>
+#include <optional>
 
 namespace reify {
 
 namespace {
-int g_global_v8_ref_count = 0;
-std::unique_ptr<v8::Platform> g_platform = nullptr;
 
-void InitializeV8() {
-  g_platform = v8::platform::NewDefaultPlatform();
-  v8::V8::InitializePlatform(g_platform.get());
-  v8::V8::Initialize();
-}
+class V8Instance {
+ public:
+  V8Instance() {
+    platform_ = v8::platform::NewDefaultPlatform();
+    v8::V8::InitializePlatform(platform_.get());
+    v8::V8::Initialize();
+  }
 
-void DisposeV8() {
-  v8::V8::Dispose();
-  v8::V8::ShutdownPlatform();
-  g_platform.reset();
+  ~V8Instance() {
+    v8::V8::Dispose();
+    v8::V8::ShutdownPlatform();
+    platform_.reset();
+  }
+
+ private:
+  std::unique_ptr<v8::Platform> platform_;
+};
+
+struct V8State {
+  std::mutex mutex;
+  int global_v8_ref_count = 0;
+  std::optional<V8Instance> instance;
+};
+
+V8State* GetV8State() {
+  static V8State v8_state;
+  return &v8_state;
 }
 
 void IncrementGlobalV8RefCount() {
-  if (g_global_v8_ref_count++ == 0) {
-    InitializeV8();
+  auto v8_state = GetV8State();
+  std::lock_guard<std::mutex> lock(v8_state->mutex);
+
+  if (v8_state->global_v8_ref_count++ == 0) {
+    if (!v8_state->instance) {
+      v8_state->instance.emplace();
+    }
   }
 }
 
 void DecrementGlobalV8RefCount() {
-  assert(g_global_v8_ref_count > 0);
+  auto v8_state = GetV8State();
+  std::lock_guard<std::mutex> lock(v8_state->mutex);
 
-  if (--g_global_v8_ref_count == 0) {
-    DisposeV8();
+  // Unfortunately, I'm experiencing crashes that I'm having a hard time
+  // debugging when we dispose of V8, and then later re-initialize it.
+  // So, we'll leave this out and just shutdown when the process ends.
+  // It's suggested by
+  // https://v8-users.narkive.com/EgxKClHl/how-to-reinitialize-v8-after-v8-v8-dispose
+  // that you shouldn't try to re-initialize V8 after disposing of it.
+  if (--v8_state->global_v8_ref_count == 0) {
+    // v8_state->instance = std::nullopt;
   }
 }
 
