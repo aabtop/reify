dbf8e1eb8bf2dffdbd449428e0088a8da3f413cb
Andrew Top <aabtop@gmail.com>
2021-03-09 22:36:52 -0800

    Make it so that Linux can find the Vulkan loader okay.

--- projects/hypo/src/ide/domain_visualizer_hypo_qt.cc
+++ projects/hypo/src/ide/domain_visualizer_hypo_qt.cc
@@ -1,12 +1,85 @@
+#include <QCoreApplication>
 #include <QVBoxLayout>
+#include <filesystem>
+#include <iostream>
 
 #include "src/ide/domain_visualizer_hypo.h"
 #include "src/ide/domain_visualizer_hypo_qt_vulkan_window.h"
 #include "src/ide/domain_visualizer_qt.h"
 
+namespace {
+
+#if defined(BAZEL_TARGET_OS) && BAZEL_TARGET_OS == LINUX
+
+// Returns an absolute path to the current executable.
+std::filesystem::path GetAbsoluteExecutableDir() {
+  return std::filesystem::absolute(
+      QCoreApplication::applicationDirPath().toStdString());
+}
+
+std::optional<std::filesystem::path> FindSoLibDir() {
+  std::optional<std::filesystem::path> previous_path;
+  for (std::filesystem::path current_path = GetAbsoluteExecutableDir();
+       current_path.has_parent_path() &&
+       (!previous_path || *previous_path != current_path);
+       current_path = current_path.parent_path()) {
+    previous_path = current_path;
+    for (const auto& p : std::filesystem::directory_iterator(current_path)) {
+      if (!std::filesystem::is_directory(p.path())) {
+        continue;
+      }
+
+      auto path_as_string = p.path().filename().string();
+      if (path_as_string.rfind("_solib", 0) == 0) {
+        return p.path();
+      }
+    }
+  }
+  return std::nullopt;
+}
+
+// Walks up the directory tree back to the root looking for vulkan.so.1 file,
+// so that we can use the one that's packaged with this application.
+std::optional<std::filesystem::path> FindVulkanSo1() {
+  auto so_lib_dir = FindSoLibDir();
+  if (!so_lib_dir) {
+    return std::nullopt;
+  }
+
+  for (const auto& p :
+       std::filesystem::recursive_directory_iterator(*so_lib_dir)) {
+    if (!std::filesystem::is_regular_file(p.path())) {
+      continue;
+    }
+
+    if (p.path().filename() == "libvulkan.so.1") {
+      return p.path();
+    }
+  }
+
+  return std::nullopt;
+}
+
+#else  // #if defined(BAZEL_TARGET_OS) && BAZEL_TARGET_OS == LINUX
+
+std::optional<std::filesystem::path> FindVulkanSo1() { return std::nullopt; }
+
+#endif  // #if defined(BAZEL_TARGET_OS) && BAZEL_TARGET_OS == LINUX
+
+}  // namespace
+
 class DomainVisualizerQtVulkan : public DomainVisualizer {
  public:
   DomainVisualizerQtVulkan(QWidget* frame) {
+    auto maybe_vulkan_so_1_path = FindVulkanSo1();
+    if (maybe_vulkan_so_1_path) {
+      // Qt doesn't know where our custom built Vulkan loader is on Linux, so
+      // we have to find it and specify it manually.
+      qputenv("QT_VULKAN_LIB", QByteArray(maybe_vulkan_so_1_path->c_str()));
+    } else {
+      std::cerr << "Could not find local build of vulkan.so.1. "
+                << "Maybe the system version will work..." << std::endl;
+    }
     q_vulkan_instance_.setLayers(QByteArrayList()
                                  << "VK_LAYER_LUNARG_standard_validation");
     if (!q_vulkan_instance_.create())
