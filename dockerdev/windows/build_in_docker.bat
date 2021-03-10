4c82b28bfe9c1dbd267caaea8db744f11c711182
Andrew Top <aabtop@gmail.com>
2021-03-10 02:00:08 -0800

    Whoops don't forge to update Windows!

--- dockerdev/windows/build_in_docker.bat
+++ dockerdev/windows/build_in_docker.bat
@@ -7,10 +7,10 @@ echo "In container, building..."
 cd %SRC_DIR%
 
 cd projects\hypo
-bazel --output_user_root=C:/_bzl build //src/ide //:hypo -c %BUILD_CONFIG% --symlink_prefix=/ --verbose_failures
+bazel --output_user_root=C:/_bzl build //:hypo //:ide -c %BUILD_CONFIG% --symlink_prefix=/ --verbose_failures
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 for /f "usebackq tokens=*" %%a in (`bazel --output_user_root=C:/_bzl info bazel-bin -c %BUILD_CONFIG%`) do (
   powershell -command "& {&'Copy-Item' %%a\hypo.exe -Destination C:/build/out/hypo.exe}"
-  CALL %SCRIPT_DIR%\package_bazel_runfiles.bat %%a\src\ide\ide.exe.runfiles\MANIFEST C:\build\out\ide ide.exe
+  CALL %SCRIPT_DIR%\package_bazel_runfiles.bat %%a\ide.exe.runfiles\MANIFEST C:\build\out\ide ide.exe
 )
