1308a9235089bf24aecaca2d57be4cf8d50b02ce
Andrew Top <aabtop@gmail.com>
2021-04-11 19:43:45 -0700

    Mark proper permissions on Linux executables.

--- dockerdev/linux/build_in_docker.sh
+++ dockerdev/linux/build_in_docker.sh
@@ -13,6 +13,7 @@ bazel build //:hypo //:ide -c $BUILD_CONFIG --symlink_prefix=/bazel- --verbose_f
 # Strip symbols from the output executable and place the results in the
 # output directory.
 strip -s /bazel-bin/hypo -o /out/hypo
+chmod +x /out/hypo
 
 IDE_DIR="/out/ide"
 if [ -d ${IDE_DIR} ]; then
@@ -22,3 +23,5 @@ fi
 mkdir -p ${IDE_DIR}
 cp -rL /bazel-bin/ide.runfiles/hypo/* ${IDE_DIR}
 strip -s ${IDE_DIR}/ide
+chmod +x ${IDE_DIR}/ide
+chmod +x ${IDE_DIR}/qt/libexec/QtWebEngineProcess
