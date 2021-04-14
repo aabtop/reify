400dae2cb385047d6b7c5a5e81e01c6823e6d5d3
Andrew Top <aabtop@gmail.com>
2021-04-13 18:24:05 -0700

    Get rid of explicit chmodding, it's not the problem.

--- dockerdev/linux/build_in_docker.sh
+++ dockerdev/linux/build_in_docker.sh
@@ -13,7 +13,6 @@ bazel build //:hypo //:ide -c $BUILD_CONFIG --symlink_prefix=/bazel- --verbose_f
 # Strip symbols from the output executable and place the results in the
 # output directory.
 strip -s /bazel-bin/hypo -o /out/hypo
-chmod +x /out/hypo
 
 IDE_DIR="/out/ide"
 if [ -d ${IDE_DIR} ]; then
@@ -23,5 +22,3 @@ fi
 mkdir -p ${IDE_DIR}
 cp -rL /bazel-bin/ide.runfiles/hypo/* ${IDE_DIR}
 strip -s ${IDE_DIR}/ide
-chmod +x ${IDE_DIR}/ide
-chmod +x ${IDE_DIR}/qt/libexec/QtWebEngineProcess
