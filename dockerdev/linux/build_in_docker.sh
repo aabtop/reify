ccc9139449f651e5582ea5de77be4fa315bbb147
Andrew Top <aabtop@gmail.com>
2021-04-14 19:38:14 -0700

    Make qt.conf *not* executable.

--- dockerdev/linux/build_in_docker.sh
+++ dockerdev/linux/build_in_docker.sh
@@ -22,3 +22,10 @@ fi
 mkdir -p ${IDE_DIR}
 cp -rL /bazel-bin/ide.runfiles/hypo/* ${IDE_DIR}
 strip -s ${IDE_DIR}/ide
+# Bazel automatically sets all files as executable, even if they're not.
+# (See https://github.com/bazelbuild/bazel/issues/6530#issuecomment-435779225).
+# This isn't a big deal, but aesthetically it's nice to have our top-level files
+# have executable permissions set correctly so that when they are listed with
+# color coding, it is evident what is executable.
+# Currently, there is only this one non-executable top-level file.
+chmod -x ${IDE_DIR}/qt.conf
