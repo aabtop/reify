4ae8da0978818a6d2df14641a9abb83449011264
Andrew Top <aabtop@gmail.com>
2021-04-16 21:32:33 -0700

    Fix build on linux.
    A header dependency was left out of the build files.

--- projects/hypo/src/ide/main.cc
+++ projects/hypo/src/ide/main.cc
@@ -1,7 +1,7 @@
 #include <QWidget>
 
-#include "domain_visualizer_hypo_qt.h"
 #include "reify/typescript_cpp_v8/ide.h"
+#include "src/ide/domain_visualizer_hypo_qt.h"
 
 int main(int argc, char* argv[]) {
   return reify::typescript_cpp_v8::StartIdeWindow(
