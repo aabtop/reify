b632687c71af65a08b464f11bfc6e06c71f5f6c5
Andrew Top <aabtop@gmail.com>
2021-06-14 22:03:19 -0700

    Fix compile error in multifile.ts.

--- projects/hypo/src/playground_workspace/example_scripts/multifile.ts
+++ projects/hypo/src/playground_workspace/example_scripts/multifile.ts
@@ -6,8 +6,8 @@ import * as TorusModule from './torus';
 export function Main() {
   return h.Union3({
     regions: [
-      TorusModule.Main(),
-      h.Transform3({ source: TorusModule.Main(), transform: h.Rotate3X(90) }),
+      TorusModule.Torus(),
+      h.Transform3({ source: TorusModule.Torus(), transform: h.Rotate3X(90) }),
       h.Transform3({ source: TungstenModule.Main(), transform: h.Rotate3Y(90) }),
     ]
   });
