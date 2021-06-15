ab4f5c2c4637f15bb23a4a293489fb8fd034aa69
Andrew Top <aabtop@gmail.com>
2021-06-14 22:40:40 -0700

    Fix up multifile support on Windows.

--- projects/hypo/src/playground_workspace/example_scripts/multifile.ts
+++ projects/hypo/src/playground_workspace/example_scripts/multifile.ts
@@ -1,7 +1,8 @@
 import * as h from 'hypo';
-import * as TungstenModule from 'tungsten';
 
+import * as TungstenModule from 'tungsten';
 import * as TorusModule from './torus';
+import * as SpiralBeamsModule from '/spiral_beams';
 
 export function Main() {
   return h.Union3({
@@ -9,6 +10,7 @@ export function Main() {
       TorusModule.Torus(),
       h.Transform3({ source: TorusModule.Torus(), transform: h.Rotate3X(90) }),
       h.Transform3({ source: TungstenModule.Main(), transform: h.Rotate3Y(90) }),
+      h.Transform3({ source: SpiralBeamsModule.Main(), transform: h.Translate3([0, 6, 0]) }),
     ]
   });
 }
