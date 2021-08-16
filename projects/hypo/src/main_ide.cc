c72f41cbab7c291c0ff9320e31f30f53d4e24f86
Andrew Top <aabtop@gmail.com>
2021-08-15 18:51:25 -0700

    Fix compile error.

--- projects/hypo/src/main_ide.cc
+++ projects/hypo/src/main_ide.cc
@@ -4,7 +4,7 @@
 #include "src/visualizer/typescript_symbol_visualizer.h"
 
 int main(int argc, char* argv[]) {
-  hypo::TypeScriptSymbolVisualizer hypo_visualizer_stack;
+  hypo::visualizer::TypeScriptSymbolVisualizer hypo_visualizer_stack;
   return reify::typescript_cpp_v8::StartIdeWindow(
       "Hypo IDE", &hypo_visualizer_stack.visualizer);
 }
