3aadc8047980d98ef70c79f5cd18683010b17513
Andrew Top <aabtop@gmail.com>
2021-11-21 16:27:48 -0800

    Fix windows not packaging all files in bazel manifest.

--- dockerdev/windows/build_in_docker.bat
+++ dockerdev/windows/build_in_docker.bat
@@ -21,6 +21,6 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 
 for /f "usebackq tokens=*" %%a in (`bazel --output_user_root=C:/_bzl info bazel-bin -c %BUILD_CONFIG%`) do (
   powershell -command "& {&'Copy-Item' %%a\hypo.exe -Destination C:/build/out/hypo.exe}"
-  CALL %SCRIPT_DIR%\package_bazel_runfiles.bat %%a\ide.exe.runfiles\MANIFEST C:\build\out ide.exe
-  CALL %SCRIPT_DIR%\package_bazel_runfiles.bat %%a\visualizer.exe.runfiles\MANIFEST C:\build\out visualizer.exe
+  CALL %SCRIPT_DIR%\package_bazel_runfiles.bat %%a\ide.exe.runfiles\MANIFEST C:\build\out hypo
+  CALL %SCRIPT_DIR%\package_bazel_runfiles.bat %%a\visualizer.exe.runfiles\MANIFEST C:\build\out hypo
 )
