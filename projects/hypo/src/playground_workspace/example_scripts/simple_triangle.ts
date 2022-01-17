a4f1ca6c18c73835f13a4e971f3e64a61dddb5c8
Andrew Top <aabtop@gmail.com>
2022-01-17 12:00:53 -0800

    Play around with adding a RNG.

--- projects/hypo/src/playground_workspace/example_scripts/simple_triangle.ts
+++ projects/hypo/src/playground_workspace/example_scripts/simple_triangle.ts
@@ -1,5 +1,43 @@
 import * as h from 'hypo';
 
-export function Main() {
-  return h.Polygon({ path: [[0, 0], [1, 0], [0.5, 1]] });
+// For the Squares cbprng our state is 64-bit integers.
+type u64 = [number, number];
+type u32 = number;
+
+type CbprngState = u64;
+
+function mult(a: u64, b: u32 | u64): u64 {
+  if (typeof b === "object") {
+
+  } else {
+
+  }
 }
+
+// "Squares" counter-based pseudo random number generator:
+//   https://arxiv.org/pdf/2004.06278.pdf
+function squares_cbprng(counter: u64, key: u64): u32 {
+  const y = counter * key;
+  const z = y + key;
+  let x = y;
+
+  x = x * x + y; x = (x >>> 32) | (x << 32); /* round 1 */
+  x = x * x + z; x = (x >>> 32) | (x << 32); /* round 2 */
+  x = x * x + y; x = (x >>> 32) | (x << 32); /* round 3 */
+  return (x * x + z) >> 32; /* round 4 */
+}
+
+export function random_i32(counter: number): number {
+  // Arbitrarily chosen.
+  const key = 0x44f13206d9c2c417;
+  return squares_cbprng(counter, key);
+}
+
+export function random_01(counter: number): number {
+  return random_i32(counter) / 2147483648.0;
+}
+
+export function Main() {
+  return h.Polygon({ path: [[random_01(11212), random_01(1)], [random_01(12), random_01(3)], [0, 0]] });
+}
+
